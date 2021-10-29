415089e5705134514800198e438b691780a1b673
Danilson Pinto <danilsonpinto@Danilson.netcabo.pt>
2021-10-29 15:59:26 +0100

    Initial commit

---
+++ src/main/java/com/db/springlogin/service/UserService.java
@@ -0,0 +1,25 @@
+package com.db.springlogin.service;
+
+
+import com.db.springlogin.model.User;
+import com.db.springlogin.model.UserDetails;
+import com.db.springlogin.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+        return user.map(UserDetails::new).get();
+    }
+}
---
+++ src/test/java/com/db/springlogin/SpringLoginApplicationTests.java
@@ -0,0 +1,14 @@
+package com.db.springlogin;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
